b6c855b595b71e251c36f67f6ecece70768b6161
Saad575757 <smartysaad59@gmail.com>
2026-09-06 17:07:09 +0500

    make it left

--- components/print/BillPrint.tsx
+++ components/print/BillPrint.tsx
@@ -35,7 +35,7 @@ export interface BillData {
 
 export function BillPrint({ bill }: { bill: BillData }) {
   return (
-    <div className="mx-auto w-[72mm] bg-white px-3 py-2 text-black">
+    <div className="mx-auto w-[60mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
