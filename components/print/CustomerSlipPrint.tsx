80dfd2bc616526dd29ac2e62e924dae77d084513
Saad575757 <smartysaad59@gmail.com>
2026-09-06 17:12:38 +0500

    negative left

--- components/print/CustomerSlipPrint.tsx
+++ components/print/CustomerSlipPrint.tsx
@@ -23,7 +23,7 @@ export interface SlipData {
 
 export function CustomerSlipPrint({ slip }: { slip: SlipData }) {
   return (
-    <div className="mx-auto w-[60mm] bg-white px-3 py-2 text-black">
+    <div className="-ml-[2mm] w-[60mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
--- components/print/TestingSlipPrint.tsx
+++ components/print/TestingSlipPrint.tsx
@@ -70,7 +70,7 @@ function EyeBlock({
 
 export function TestingSlipPrint({ slip }: { slip: TestingSlipPrintData }) {
   return (
-    <div className="mx-auto w-[60mm] bg-white px-3 py-2 text-black">
+    <div className="-ml-[2mm] w-[60mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
