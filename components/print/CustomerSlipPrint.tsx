a3a17af628eeef3305070d155248f6913f762663
Saad575757 <smartysaad59@gmail.com>
2026-09-06 17:21:14 +0500

    negative left 12mm

--- components/print/CustomerSlipPrint.tsx
+++ components/print/CustomerSlipPrint.tsx
@@ -23,7 +23,7 @@ export interface SlipData {
 
 export function CustomerSlipPrint({ slip }: { slip: SlipData }) {
   return (
-    <div className="-ml-[20mm] w-[60mm] bg-white px-3 py-2 text-black">
+    <div className="-ml-[12mm] w-[60mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
--- components/print/EyePrescriptionPrint.tsx
+++ components/print/EyePrescriptionPrint.tsx
@@ -64,7 +64,7 @@ export function EyePrescriptionPrint({
   data: EyePrescriptionPrintData;
 }) {
   return (
-    <div className="-ml-[20mm] w-[60mm] bg-white px-3 py-2 text-black">
+    <div className="-ml-[12mm] w-[60mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
