f3d1bf02123a621f948651a77f11a8aabab68755
Saad575757 <smartysaad59@gmail.com>
2026-09-06 16:32:21 +0500

    done

--- components/print/CustomerSlipPrint.tsx
+++ components/print/CustomerSlipPrint.tsx
@@ -23,7 +23,7 @@ export interface SlipData {
 
 export function CustomerSlipPrint({ slip }: { slip: SlipData }) {
   return (
-    <div className="mx-auto w-[72mm] bg-white px-3 py-2 text-black">
+    <div className="mx-auto w-[80mm] bg-white px-3 py-2 text-black">
       <div className="flex justify-center">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
