42b8fe4515d25927bddd8da4d55157bd9fc621c0
Saad575757 <smartysaad59@gmail.com>
2026-09-08 00:49:37 +0500

    changes done

--- components/print/BillPrint.tsx
+++ components/print/BillPrint.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { format } from "date-fns";
+import { Facebook, Instagram } from "lucide-react";
 import { formatCurrency } from "@/lib/utils";
 
 export interface BillData {
@@ -135,13 +136,7 @@ export function BillPrint({ bill }: { bill: BillData }) {
         </div>
       </div>
 
-      {bill.qrCode && (
-        <div className="mt-3 flex flex-col items-center">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={bill.qrCode} alt="QR code" className="h-24 w-24" />
-          <p className="mt-1 text-[10px]">Scan to verify order</p>
-        </div>
-      )}
+      
 
       <div className="my-3 border-t border-dashed border-gray-400" />
 
@@ -166,6 +161,28 @@ export function BillPrint({ bill }: { bill: BillData }) {
         Note: Article not collected within 30 days shall
         <br />
         be considered unclaimed.
+      </div>
+
+      <div className="mt-3 flex items-start justify-center gap-5 text-black">
+        <div className="flex flex-col items-center gap-0.5">
+          <Facebook className="h-4 w-4" />
+          <span className="text-[8px]">Facebook</span>
+        </div>
+        <div className="flex flex-col items-center gap-0.5">
+          <Instagram className="h-4 w-4" />
+          <span className="text-[8px]">Instagram</span>
+        </div>
+        <div className="flex flex-col items-center gap-0.5">
+          <svg
+            viewBox="0 0 24 24"
+            className="h-4 w-4"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z" />
+          </svg>
+          <span className="text-[8px]">TikTok</span>
+        </div>
       </div>
     </div>
   );
